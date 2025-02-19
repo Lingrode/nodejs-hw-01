f01a739f8fc47f6a6cf1d987cffecf0b180f991f
Illia Hrymalo <grimaloilya@gmail.com>
2025-02-19 02:05:47 -0700

    generating contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,24 @@
-const generateContacts = async (number) => {};
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const data = await readContacts();
+    const contacts = JSON.parse(data);
+
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+
+    const updatedContacts = [...contacts, ...newContacts];
+
+    await writeContacts(JSON.stringify(updatedContacts));
+
+    console.log('Contacts successfully generated!');
+  } catch (error) {
+    console.error('Error while generating contacts: ', error.message);
+  }
+};
 
 generateContacts(5);
