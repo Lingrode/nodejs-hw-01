ffa7869a48742cc9e46483ab73ee1e1ad1df7543
Illia Hrymalo <grimaloilya@gmail.com>
2025-02-19 02:14:10 -0700

    add one contact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,20 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const data = await readContacts();
+    const contacts = JSON.parse(data);
+
+    const newContacts = [...contacts, createFakeContact()];
+
+    await writeContacts(JSON.stringify(newContacts));
+
+    console.log('Contact added succefully!');
+  } catch (error) {
+    console.error('Error while adding contact: ', error.message);
+  }
+};
 
 addOneContact();
