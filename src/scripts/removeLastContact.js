1cbb9268b37dbbef3c3c4f549e585d285c59fe31
Illia Hrymalo <grimaloilya@gmail.com>
2025-02-19 22:47:59 -0700

    remove last contact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,23 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const data = await readContacts();
+    const contacts = JSON.parse(data);
+
+    if (contacts.length === 0) {
+      console.log('File already empty!');
+      return;
+    }
+
+    const removedContact = contacts.pop();
+
+    await writeContacts(JSON.stringify(contacts));
+    console.log('Removed contact:\n', removedContact);
+  } catch (error) {
+    console.error('Error while removing contact: ', error.message);
+  }
+};
 
 removeLastContact();
