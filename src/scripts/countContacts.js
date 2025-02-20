ac27f57ccd2d173722b36de86783b438eb35fc6c
Illia Hrymalo <grimaloilya@gmail.com>
2025-02-19 22:28:49 -0700

    count contacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,13 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const data = await readContacts();
+    const contacts = JSON.parse(data);
+    return `Total contacts: ${contacts.length}`;
+  } catch (error) {
+    console.log('Error while counting contacts: ', error.message);
+  }
+};
 
 console.log(await countContacts());
