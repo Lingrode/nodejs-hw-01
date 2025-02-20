b4d2028d6e62149dd6ff4decfc98188662b79b8f
Illia Hrymalo <grimaloilya@gmail.com>
2025-02-19 22:25:03 -0700

    get all contacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const data = await readContacts();
+    return JSON.parse(data);
+  } catch (error) {
+    console.log('Error while getting contacts: ', error.message);
+  }
+};
 
 console.log(await getAllContacts());
